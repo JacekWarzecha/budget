85b4a93ade18e4e7f466cb2ceaecfd9c9a3d4db3
JacekWarzecha <drjackys@gmail.com>
2023-06-29 12:00:18 +0200

    added working navigation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Navigation, List, ListItem, StyledNavlink } from "./common/Navigation";
+import { DataBase } from "./features/dataBase";
 import Budget from "./features/budget/Budget";
 
-export default () => <Budget />;
+export default () => (
+  <HashRouter>
+    <Navigation>
+      <List>
+        <ListItem>
+          <StyledNavlink to="/specyfikacja">budżet</StyledNavlink>
+        </ListItem>
+        <ListItem>
+          <StyledNavlink to="/tablica">tablica danych</StyledNavlink>
+        </ListItem>
+      </List>
+      <Routes>
+        <Route path="/specyfikacja" element={<Budget />} />
+        <Route path="/tablica" element={<DataBase />} />
+        <Route path="/" element={<Navigate to="/specyfikacja" replace />} />
+      </Routes>
+    </Navigation>
+  </HashRouter>
+);
---
+++ src/common/Navigation/index.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Navigation = styled.nav``;
+
+export const List = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+export const ListItem = styled.li``;
+
+export const StyledNavlink = styled(NavLink)``;
